b716fcb29d749289ee5779baf6b61fb2a9b0714b
Tiisetso-Polori <118134452+Tiisetso-Polori@users.noreply.github.com>
2024-04-18 21:23:12 +0200

    Working code, added most of my shoots. I need to do more improvements.

--- Profile/js/galleryRunner.js
+++ Profile/js/galleryRunner.js
@@ -102,6 +102,13 @@ var GalleryRunner = {
             //cover:nippy,
             dir: '../Profile/react-gallery/src/resrc/nippy/',
             photos:['0.jpg','1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg','12.jpg','13.jpg','14.jpg','15.jpg','16.jpg','17.jpg','18.jpg','19.jpg','20.jpg','21.jpg','22.jpg','23.jpg','24.jpg']
+        },
+        {
+            id:16,
+            header:'Muse',
+            //cover:muse,
+            dir: '../Profile/react-gallery/src/resrc/muse/',
+            photos:['0.jpg','1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg','12.jpg']
         }
     ],
     
