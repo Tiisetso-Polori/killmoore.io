b30190a32b4a60ad45b249dfd363bf7dbb11342a
Tiisetso-Polori <118134452+Tiisetso-Polori@users.noreply.github.com>
2024-04-21 17:25:24 +0200

    Adding products code.

--- Profile/js/galleryRunner.js
+++ Profile/js/galleryRunner.js
@@ -93,9 +93,8 @@ var GalleryRunner = {
             header:'Shoot 3',
             //cover:shoot3,
             dir: '../Profile/react-gallery/src/resrc/shoot 3/',
-            photos:['0.jpg','1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg']
+            photos:['0.jpg','1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg','12.jpg','13.jpg','14.jpg','15.jpg','16.jpg','17.jpg','18.jpg','19.jpg','20.jpg','21.jpg','22.jpg','23.jpg','24.jpg','25.jpg']
         },
-        ,
         {
             id:15,
             header:'Nippy',
